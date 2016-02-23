0af8c9718dcde18ea8380c4bdae5e1aec4785d2a
andy1138 <andy@hicks.net>
2016-02-23 13:49:05 +0000

    init empty play frontend

---
+++ project/FrontendBuild.scala
@@ -0,0 +1,56 @@
+import sbt._
+
+object FrontendBuild extends Build with MicroService {
+  import scala.util.Properties.envOrElse
+
+  val appName = "address-lookup-demo"
+  val appVersion = envOrElse("ADDRESS_LOOKUP_DEMO_FRONTEND_VERSION", "999-SNAPSHOT")
+
+  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
+}
+
+private object AppDependencies {
+  import play.PlayImport._
+  import play.core.PlayVersion
+
+  private val playHealthVersion = "1.1.0"    
+  private val playJsonLoggerVersion = "2.1.1"      
+  private val frontendBootstrapVersion = "6.1.0"
+  private val govukTemplateVersion = "4.0.0"
+  private val playUiVersion = "4.9.0"
+  private val playPartialsVersion = "4.2.0"
+  private val playAuthorisedFrontendVersion = "4.5.0"
+  private val playConfigVersion = "2.0.1"
+  
+  val compile = Seq(
+    ws,
+    "uk.gov.hmrc" %% "frontend-bootstrap" % frontendBootstrapVersion,
+    "uk.gov.hmrc" %% "play-partials" % playPartialsVersion,
+    "uk.gov.hmrc" %% "play-authorised-frontend" % playAuthorisedFrontendVersion,
+    "uk.gov.hmrc" %% "play-config" % playConfigVersion,
+    "uk.gov.hmrc" %% "play-json-logger" % playJsonLoggerVersion,
+    "uk.gov.hmrc" %% "govuk-template" % govukTemplateVersion,
+    "uk.gov.hmrc" %% "play-health" % playHealthVersion,
+    "uk.gov.hmrc" %% "play-ui" % playUiVersion
+  )
+
+  trait TestDependencies {
+    lazy val scope: String = "test"
+    lazy val test : Seq[ModuleID] = ???
+  }
+
+  object Test {
+    def apply() = new TestDependencies {
+      override lazy val test = Seq(
+        "org.scalatest" %% "scalatest" % "2.2.2" % scope,
+        "org.pegdown" % "pegdown" % "1.4.2" % scope,
+        "org.jsoup" % "jsoup" % "1.7.3" % scope,
+        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+      )
+    }.test
+  }
+
+  def apply() = compile ++ Test()
+}
+
+
